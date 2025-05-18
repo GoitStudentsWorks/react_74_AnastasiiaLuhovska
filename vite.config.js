cd9e32952e07b40aa279d35c9081e44d74f9224c
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2025-05-18 21:45:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {sourcemap: true}
+  build: {sourcemap: true},
+  base: "/react_74_AnastasiiaLuhovska/"
 })
